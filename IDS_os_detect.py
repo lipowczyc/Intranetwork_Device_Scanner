121c32b623281244759e816b3a81f8982fbebf64
lipowczyc <107719344+lipowczyc@users.noreply.github.com>
2025-01-16 16:03:09 +0100

    Update IDS_os_detect.py

--- IDS_os_detect.py
+++ IDS_os_detect.py
@@ -1,113 +1,187 @@
 import nmap3
 import json
 import re
+import pathlib
 import datetime
-import pprint
-
-# folder_path = str(input('Enter absolute: path/to/your/folder : '))
-# # D:\\Users\\lipow\\PycharmProjects\\Intranetwork_Device_Scanner\\dupa
-# if not os.path.exists(folder_path):
-#     os.mkdir(folder_path)
-#     print(f"Folder '{folder_path}' created.")
-# else:
-#     print(f"Folder '{folder_path}' already exists.")
-#
-# default_IDS_folder_path = folder_path+"/netdiscovery"
-#
-# if not os.path.exists(folder_path):
-#     os.mkdir(default_IDS_folder_path)
-#     print(f"Folder netdiscovery created in {folder_path}")
-# else:
-#     print(f"Folder netdiscovery already exists.")
-# network_name = str(input("Enter network/company name: "))
-network_name = "Efferta"
-timestamp = datetime.datetime.now()
-# current_datetime = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-nmap = nmap3.Nmap()
-ip_to_scan = "192.168.7.0/24"  # str(input("Enter an ip address to scan or subnet: ")
 
 
-def ids_os():
-    file_name = "D:\\Sylwia\\python\\IDS\\IDS_OS_scan_0.json"
-    # with open(file_name, "w") as outfile:
-    #     os_results = nmap.nmap_os_detection(ip_to_scan)
-    #     json.dump(os_results, outfile, indent=4)
+class IDSosDetect:
+    default_folder_path = ""
+    network_name: str = ""
+    file_name: str = ""
+    ip_to_scan: str = ""
+    current_datetime = ""
+    output_dict = {
+        "network_name": "",
+        "current_datetime": "",
+        "scan_id": 0,
+        "mac_addr": "",
+        "hostname": "",
+        "full_hostname": "",
+        "machine_name": "",
+        "machine_ip": ""
+    }
 
-    with open(file_name, "r") as new_file:
-        os_results_from_file = json.load(new_file)
-        # print(os_results_from_file)
-        ip_patt = r'[0-9]+(?:\.[0-9]+){3}'
+    def start(self):
+        self.initial_info()
+        self.do_scan()
+        self.collect_data()
+        self.give_output(IDSosDetect.output_dict)
+
+    def initial_info(self):
+        user_input_path = pathlib.Path(str(input('Enter place to save data - absolute: path/to/your/folder : \n')))
+        self.check_path(user_input_path)
+        default_folder_path = pathlib.Path.joinpath(user_input_path, "netdiscovery")
+        self.check_path(default_folder_path)
+        IDSosDetect.default_folder_path = default_folder_path
+        IDSosDetect.network_name = str(input("Enter network/company name: "))
+        timestamp = datetime.datetime.now()
+        IDSosDetect.current_datetime = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        stamp_for_file = timestamp.strftime("%y%m%d_%H_%M")
+        IDSosDetect.file_name = pathlib.Path.joinpath(default_folder_path, f"IDS_OS_scan_{stamp_for_file}.json")
+        ip_is_correct = False
+        while not ip_is_correct:
+            user_input = str(input("Enter an ip address to scan or subnet: "))
+            ip_is_correct = self.check_ip(user_input)
+            if not ip_is_correct:
+                print("Address is incorrect, try again")
+        IDSosDetect.ip_to_scan = user_input
+
+    @staticmethod
+    def check_path(path):
+        if not path.exists():
+            path.mkdir(exist_ok=True)
+            print(f"Folder '{path}' created.")
+        else:
+            print(f"Folder '{path.exists()}' already exists.")
+
+    @staticmethod
+    def do_scan():
+        nmap = nmap3.Nmap()
+        with open(IDSosDetect.file_name, "w") as outfile:
+            os_results = nmap.nmap_os_detection(IDSosDetect.ip_to_scan)
+            json.dump(os_results, outfile, indent=4)
+
+    def collect_data(self):
+        with open(IDSosDetect.file_name, "r") as new_file:
+            scan_id = 1
+            os_results_from_file = json.load(new_file)
+            for result_key, result_value in os_results_from_file.items():
+                if self.check_ip(result_key):
+                    ip_add = result_key
+                if result_key is ip_add:
+                    if self.check_result_value(result_value):
+                        mac_addr, mac_vendor = self.get_macinfo(result_value)
+                        hostname, full_hostname = self.get_hostname(result_value, ip_add)
+
+                        result_os = self.os_match(result_value)
+                        if result_os:
+                            for list_item in range(1):
+                                machine_name = result_os[list_item].get("name")
+                        else:
+                            if mac_vendor:
+                                machine_name = mac_vendor
+                            else:
+                                machine_name = "not found"
+                        up_out_dict = {
+                            "network_name": IDSosDetect.network_name,
+                            "current_datetime": IDSosDetect.current_datetime,
+                            "scan_id": scan_id,
+                            "mac_addr": mac_addr,
+                            "hostname": hostname,
+                            "full_hostname": full_hostname,
+                            "machine_name": machine_name,
+                            "machine_ip": ip_add
+                        }
+                        scan_id += 1
+                        IDSosDetect.output_dict.update(up_out_dict)
+
+    @staticmethod
+    def check_ip(key):
+        ip_patt = (r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+                   r"([/][0-3][0-2]?|[/][1-2][0-9]|[/][0-9])?$")
+        match_ip = re.findall(ip_patt, key)
+        if match_ip:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def check_result_value(value):
         empty_result = {'osmatch': {}, 'ports': [], 'hostname': [], 'macaddress': None}
-        scan_id = 1
-        for result_key, result_value in os_results_from_file.items():
-            if re.findall(ip_patt, result_key):
-                ip_add = result_key
-            if result_key is ip_add:
-                current_datetime = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-                if result_value != empty_result:
-                    # print(ip_add)
-                    macaddress = result_value.get("macaddress")
-                    if macaddress:
-                        addr = macaddress.get("addr")
-                        vendor = macaddress.get("vendor")
-                        if vendor:
-                            vendor = vendor
-                        else:
-                            vendor = "not found"
-                    else:
-                        addr = "00:00:00:00:00:00"
+        if value != empty_result:
+            return True
 
-                    hostname_list = result_value.get("hostname")
-                    if hostname_list:
-                        for it_em in range(1):
-                            hostname = hostname_list[it_em].get("name")
-                    else:
-                        hostname = ip_add
+    @staticmethod
+    def get_macinfo(value):
+        macinfo = value.get("macaddress")
+        if macinfo:
+            result_addr = macinfo.get("addr")
+            result_vendor = macinfo.get("vendor")
+            if result_vendor:
+                result_vendor = result_vendor
+            else:
+                result_vendor = "not found"
+        else:
+            result_addr = "00:00:00:00:00:00"
+            result_vendor = "not found"
+        return [result_addr, result_vendor]
 
-                    osmatch = result_value.get("osmatch")
-                    if osmatch:
-                        for list_item in range(1):  # range(len(osmatch))
-                            item_name = osmatch[list_item].get("name")
+    @staticmethod
+    def get_hostname(value, ip_addr):
+        hostname_list = value.get("hostname")
+        if hostname_list:
+            for it_em in range(1):
+                full_host = hostname_list[it_em].get("name")
 
-                    else:
-                        if vendor:
-                            item_name = vendor
-                        else:
-                            item_name = "not found"
+                split_hostname = full_host.split(".")
+            host = split_hostname[0]
+        else:
+            host = ip_addr
+            full_host = "not found"
+        return [host, full_host]
 
-                    pattern_dictionary = {
-                        "action": "inventory",
-                        "content": {
-                            "accesslog": {
-                                "logdate": current_datetime  # variable with date: "2024-11-05 20:41:55"
-                            },
-                            "hardware": {
-                                "name": hostname
-                            },
-                            "networks": [
-                                {
-                                    "description": "eth0",
-                                    "ipaddress": ip_add,
-                                    "mac": addr
-                                }
-                            ],
-                            "operatingsystem": {
-                                # "dns_domain": "",  # eg "mawi.local"
-                                "fqdn": hostname,  # eg  "mawitech.mawi.local"
-                                "full_name": item_name
-                            },
-                            "versionclient": "GLPI-Inventory_v1.5-1"
-                        },
-                        "deviceid": f"{network_name}_{scan_id}",  # f"{network_name}_{scan_id}"
-                        "itemtype": "Computer"
+    @staticmethod
+    def os_match(value):
+        osmatch = value.get("osmatch")
+        return osmatch
+
+    @staticmethod
+    def give_output(dicto):
+        pattern_dictionary = {
+            "action": "inventory",
+            "content": {
+                "accesslog": {
+                    "logdate": dicto["current_datetime"]
+                },
+                "hardware": {
+                    "name": dicto["hostname"]
+                },
+                "networks": [
+                    {
+                        "description": "eth0",
+                        "ipaddress": dicto["machine_ip"],
+                        "mac": dicto["mac_addr"]
                     }
-                    scan_id += 1
-                    # device_file_name = f"D:\\Sylwia\\python\\IDS\\{ip_add}.json"
-                    # with open(device_file_name, "w") as id_file:
-                    #     json.dump(pattern_dictionary, id_file, indent=4)
+                ],
+                "operatingsystem": {
+                    "fqdn": dicto["full_hostname"],
+                    "full_name": dicto["machine_name"]
+                },
+                "versionclient": "GLPI-Inventory_v1.5-1"
+            },
+            "deviceid": f"{dicto["network_name"]}_{dicto["scan_id"]}",
+            "itemtype": "Computer"
+        }
 
-                    print(f"{ip_add}: {pattern_dictionary}")
-                    # pprint.pprint(pattern_dictionary, )
+        device_file_name = pathlib.Path.joinpath(IDSosDetect.default_folder_path, f"{dicto["machine_ip"]}.json")
+        with open(device_file_name, "w") as id_file:
+            json.dump(pattern_dictionary, id_file, indent=4)
 
 
-ids_os()
+def begin_process():
+    cu_object = IDSosDetect()
+    cu_object.start()
+
+
+begin_process()
